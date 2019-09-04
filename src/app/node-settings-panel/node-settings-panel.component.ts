8994aa6d5d78455afe4d5f72060bef8a2dfda171
Alex <koralexpit@gmail.com>
2019-09-04 15:08:02 +0300

    test design sidebar

--- src/app/node-settings-panel/node-settings-panel.component.ts
+++ src/app/node-settings-panel/node-settings-panel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { ModelService } from '../services/model.service';
 
 @Component({
   selector: 'app-node-settings-panel',
@@ -6,26 +9,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./node-settings-panel.component.scss']
 })
 export class NodeSettingsPanelComponent implements OnInit {
-  asrType = ["Слитное распознавание", "Распознавание по грамматике"]
-  step = 0;
 
-  constructor() { }
+
+
+  asrType = ['Слитное распознавание', 'Распознавание по грамматике']
+  // step = 0;
+  model;
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruits: any[] = [
+    { name: 'Lemon' },
+    { name: 'Lime' },
+    { name: 'Apple' },
+  ];
+
+  panelOpenState = false;
+
+  constructor(private modelService: ModelService) { }
 
   ngOnInit() {
-  }
-  
-  setStep(index: number) {
-    this.step = index;
-  }
-
-  nextStep() {
-    this.step++;
+    this.model = this.modelService.model;
+    console.log('TEST', this.model);
+    
   }
 
-  prevStep() {
-    this.step--;
+  // setStep(index: number) {
+  //   this.step = index;
+  // }
+
+  // nextStep() {
+  //   this.step++;
+  // }
+
+  // prevStep() {
+  //   this.step--;
+  // }
+
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.fruits.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
   }
 
+  remove(fruit: any): void {
+    console.log('TEST', this.modelService.model);
+    const index = this.fruits.indexOf(fruit);
 
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+    
+    
+    console.log('UpDATe',this.modelService.model );
+  }
 
 }
